4eb6a766c2523aeed8309d23a4615ab2a612e728
Gabriel Moraes <107960094+GMProf@users.noreply.github.com>
2022-10-20 23:13:35 -0300

    commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
+
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value
+    input.value += value
+  })
+})
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = ""
+  input.focus()
+})
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault()
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key
+    return
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1)
+  }
+  if (ev.key === "Enter") {
+    calculate()
+  }
+})
+
+document.getElementById("equal").addEventListener("click", calculate)
+
+function calculate() {
+  resultInput.value = "ERRO !"
+  resultInput.classList.add("error")
+  const result = eval(input.value)
+  resultInput.value = result
+  resultInput.classList.remove("error")
+}
+
+// document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
+//   const button = ev.currentTarget
+//   if (button.innerText === "Copy") {
+//     button.innerText = "Copied!"
+//     button.classList.add("success")
+//     navigator.clipboard.writeText(resultInput.value)
+//   } else {
+//     button.innerText = "Copy"
+//     button.classList.remove("success")
+//   }
+// })
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#3d6caa")
+    main.dataset.theme = "light"
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#3d6caa")
+    main.dataset.theme = "dark"
+  }
+})
+
+const getHours = () => {
+    const h1 = document.getElementsByClassName('clock-h1')[0]
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const hour = hours < 10 ? `0${hours}` : hours
+    const minute = minutes < 10 ? `0${minutes}` : minutes
+    const second = seconds < 10 ? `0${seconds}` : seconds
+    h1.innerText = `${hour}:${minute}:${second}`
+  }
+  
+  setInterval(() => {
+    getHours()
+  }, 1000)
+
